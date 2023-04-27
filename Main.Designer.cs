066d555b9f7470e54feaa71d40db475927e72b9d
Z3P0L <worldtico@gmail.com>
2023-04-27 09:32:46 -0500

    init

---
+++ Main.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace PasswordScrambler
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnScramble = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.llblCreator = new System.Windows.Forms.LinkLabel();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(209, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Password Scrambler";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.Location = new System.Drawing.Point(14, 46);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(203, 16);
+            this.lblDescription.TabIndex = 1;
+            this.lblDescription.Text = "Alternative to password manager";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(17, 103);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(0);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(204, 25);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Text = "Type your normal password";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(17, 175);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(200, 64);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(14, 148);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(39, 13);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "Output";
+            // 
+            // btnScramble
+            // 
+            this.btnScramble.Location = new System.Drawing.Point(17, 265);
+            this.btnScramble.Name = "btnScramble";
+            this.btnScramble.Size = new System.Drawing.Size(75, 23);
+            this.btnScramble.TabIndex = 5;
+            this.btnScramble.Text = "Scramble";
+            this.btnScramble.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(142, 265);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // llblCreator
+            // 
+            this.llblCreator.AutoSize = true;
+            this.llblCreator.Location = new System.Drawing.Point(342, 18);
+            this.llblCreator.Name = "llblCreator";
+            this.llblCreator.Size = new System.Drawing.Size(230, 13);
+            this.llblCreator.TabIndex = 7;
+            this.llblCreator.TabStop = true;
+            this.llblCreator.Text = "https://github.com/Z3P0L/password-scrambler";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.BackColor = System.Drawing.Color.Transparent;
+            this.btnCopy.BackgroundImage = global::PasswordScrambler.Properties.Resources.copy;
+            this.btnCopy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCopy.Location = new System.Drawing.Point(188, 148);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(29, 23);
+            this.btnCopy.TabIndex = 8;
+            this.btnCopy.UseVisualStyleBackColor = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 307);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.llblCreator);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnScramble);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnScramble;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.LinkLabel llblCreator;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
